9a49546da59a9892cf49478a927b8557a58e548c
harsh <harsh.chouhan010@gmail.com>
2023-10-17 23:03:33 -0700

    Harsh commit latest with 6

---
+++ src/DashboardCard.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from "react";
+import PropTypes from 'prop-types';
+import { Grid, Typography } from "@mui/material";
+const DashboardCard = ({ title, count, img }) => {
+    return (
+        <>
+            <Grid container>
+                <div className="card purple-card">
+                    <div className="front-card">
+                        <div className="card-img">
+                            <img src={img} />
+                        </div>
+                        <div>
+                            <Typography variant="h6" component="p">
+                                {count}
+                            </Typography>
+                            <Typography variant="h3" component="h3">
+                                {title}
+                            </Typography>
+                        </div>
+                    </div>
+                </div>
+            </Grid>
+        </>
+    )
+}
+DashboardCard.propTypes = {
+    title: PropTypes.string,
+    count: PropTypes.string,
+}
+export default DashboardCard;
